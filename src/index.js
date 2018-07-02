cf73f816f0186715427130818a469931d0a228a2
Emina Krehic <eminakrehic10@gmail.com>
2018-07-02 12:58:47 +0200

    prefix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import Game from './components/Game';
-import './css/style.css';
+import './style/style.css';
 
 
 render(<Game />, document.querySelector("#main"));
